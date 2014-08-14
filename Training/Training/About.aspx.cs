ae3c8aee890871931c4c6174de4768012e3734a0
PizzaMatt <matt1.cookman@gmail.com>
2014-08-14 10:12:36 -0700

    Holla Holla get $

--- Training/Training/About.aspx.cs
+++ Training/Training/About.aspx.cs
@@ -5,11 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Training
+namespace WebApplication5
 {
-    public partial class About : Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Buildingchoose_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
