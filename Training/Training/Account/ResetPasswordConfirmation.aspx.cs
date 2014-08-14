76ffed571df374c8ab7b17f85cdd4f51192f4c79
theomnix <ajmtallone@gmail.com>
2014-08-14 09:59:53 -0700

    Added initial project

---
+++ Training/Training/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Training.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Training/Training/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Training.Startup))]
+namespace Training
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
