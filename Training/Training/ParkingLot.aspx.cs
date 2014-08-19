1c7bd9d8dfd75347821963196ff5b68deda34229
PizzaMatt <matt1.cookman@gmail.com>
2014-08-19 10:31:20 -0700

    Can add parking lots with spots

--- Training/Training/ParkingLot.aspx.cs
+++ Training/Training/ParkingLot.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -18,13 +20,93 @@ namespace Training
 
         protected void TEST_Click(object sender, EventArgs e)
         {
-            
-            Page.ClientScript.RegisterStartupScript(this.GetType(), "msgbox", "alert(it's clicked);\n", true);
+            int handi = Convert.ToInt32(Handicap.Text);
+            int comp = Convert.ToInt32(Compact.Text);
+            int reg = Convert.ToInt32(Regular.Text);
+            int total = handi + comp + reg;
+            SqlConnection conn = new SqlConnection(SqlDataSource1.ConnectionString);
+            SqlCommand cmd = new SqlCommand("submitparkinglot", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@NumberOfSpots", SqlDbType.Int).Value = total;
+            if (Covered.SelectedValue.Equals("YES"))
+            {
+                cmd.Parameters.Add("@Covered", SqlDbType.Bit).Value = 1;
+            }
+            else
+            {
+                cmd.Parameters.Add("@Covered", SqlDbType.Bit).Value = 0;
+            }
 
-            Response.Write("<script type=text/javascript>");
-            Response.Write("alert(You found it!!)");
-            Response.Write("</script>");
-            System.Web.HttpContext.Current.Response.Write("IT's clicked");
+            cmd.Parameters.Add("@Compact", SqlDbType.Int).Value = comp;
+            cmd.Parameters.Add("@Handicapped", SqlDbType.Int).Value = handi;
+            cmd.Parameters.Add("@Building", SqlDbType.NChar).Value = Buildingchoose.SelectedValue;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            
+            conn.Close();
+            populateSpots();
+           
+            
+        }
+
+        private void populateSpots()
+        {
+            SqlConnection con = new SqlConnection(SqlDataSource1.ConnectionString);
+            
+            SqlCommand cmd = new SqlCommand("getnewestlot", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader read = cmd.ExecuteReader();
+            read.Read();
+            int lotnumber = read.GetInt32(0);
+            int totalspots = read.GetInt32(1);
+            int compy = read.GetInt32(3);
+            int handi = read.GetInt32(4);
+            con.Close();
+
+            SqlCommand insert = new SqlCommand("newspot", con);
+
+            for (int i = 0; i <= compy; i++)
+            {
+                con = new SqlConnection(SqlDataSource1.ConnectionString);
+                insert = new SqlCommand("newspot",con);
+            insert.CommandType = CommandType.StoredProcedure;
+            insert.Parameters.Add("@LotNumber", SqlDbType.Int).Value = lotnumber;
+            insert.Parameters.Add("@Compact", SqlDbType.Bit).Value = 1;
+            insert.Parameters.Add("@Handicapped", SqlDbType.Bit).Value = 0;
+            con.Open();
+            insert.ExecuteNonQuery();
+            con.Close();
+            }
+
+            for (int i = 0; i <= handi; i++)
+            {
+                con = new SqlConnection(SqlDataSource1.ConnectionString);
+                insert = new SqlCommand("newspot",con);
+                insert.CommandType = CommandType.StoredProcedure;
+                insert.Parameters.Add("@LotNumber", SqlDbType.Int).Value = lotnumber;
+                insert.Parameters.Add("@Compact", SqlDbType.Bit).Value = 0;
+                insert.Parameters.Add("@Handicapped", SqlDbType.Bit).Value = 1;
+                con.Open();
+                insert.ExecuteNonQuery();
+                con.Close();
+            }
+
+            for (int i = 0; i <= totalspots-(compy+handi); i++)
+            {
+                con = new SqlConnection(SqlDataSource1.ConnectionString);
+                
+                insert = new SqlCommand("newspot",con);
+                insert.CommandType = CommandType.StoredProcedure;
+                insert.Parameters.Add("@LotNumber", SqlDbType.Int).Value = lotnumber;
+                insert.Parameters.Add("@Compact", SqlDbType.Bit).Value = 0;
+                insert.Parameters.Add("@Handicapped", SqlDbType.Bit).Value = 0;
+                con.Open();
+                insert.ExecuteNonQuery();
+                con.Close();
+            }
+                
+
         }
     }
 }
