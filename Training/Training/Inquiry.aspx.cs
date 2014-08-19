1be573a96d0e2da3f20fe307318ad3b7f96a82c5
theomnix <ajmtallone@gmail.com>
2014-08-19 11:30:03 -0700

    Implemented the database, but it still contains several glitches

--- Training/Training/Inquiry.aspx.cs
+++ Training/Training/Inquiry.aspx.cs
@@ -10,35 +10,116 @@ using System.Web.Services;
 
 namespace Training
 {
-    public partial class Inquiry : Page
+    public partial class Inquiry : System.Web.UI.Page, IPostBackEventHandler
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-        }
-
-        protected String Submit(object sender, EventArgs e)
+        public void RaisePostBackEvent(string eventArgument)
         {
             SqlConnection myConnection = new SqlConnection("Data Source=172.50.32.84\\SQLEXPRESS; Server=MININT-3L8403G\\SQLEXPRESS; Initial Catalog=Apartment; User ID=test; Password=1234");
+            myConnection.Open();
 
             String counter = hdnfield.Value;
             int count = int.Parse(counter);
-           
 
-            myConnection.Open();
-            /*
-            String info = "INSERT INTO Flats VALUES('A', '101', '2', '3', 0, '1500')";
+            if (count == 2)
+            {
+                String inquiry1 = "INSERT INTO Flats VALUES('" + HiddenField25.Value + "', '" + HiddenField26.Value + "', '" +
+                                   HiddenField27.Value + "', '" + HiddenField28.Value + "', '" + HiddenField29.Value + "', '" +
+                                   HiddenField30.Value + "')";
 
-            SqlCommand myCommand = new SqlCommand(info, myConnection);
+                SqlCommand myCommand = new SqlCommand(inquiry1, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            else if (count == 3)
+            {
+                String inquiry1 = "INSERT INTO Flats VALUES(" + HiddenField25.Value + ", " + HiddenField26.Value + ", " +
+                                   HiddenField27.Value + ", " + HiddenField28.Value + ", " + HiddenField29.Value + ", " +
+                                   HiddenField30.Value + ")";
+                String inquiry2 = "INSERT INTO Flats VALUES(" + HiddenField1.Value + ", " + HiddenField2.Value + ", " +
+                                   HiddenField3.Value + ", " + HiddenField4.Value + ", " + HiddenField5.Value + ", " +
+                                   HiddenField6.Value + ")";
 
-            myCommand.ExecuteNonQuery();
-            */
+                SqlCommand myCommand = new SqlCommand(inquiry1, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry2, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            else if (count == 4)
+            {
+                String inquiry1 = "INSERT INTO Flats VALUES(" + HiddenField25.Value + ", " + HiddenField26.Value + ", " +
+                                   HiddenField27.Value + ", " + HiddenField28.Value + ", " + HiddenField29.Value + ", " +
+                                   HiddenField30.Value + ")";
+                String inquiry2 = "INSERT INTO Flats VALUES(" + HiddenField1.Value + ", " + HiddenField2.Value + ", " +
+                                   HiddenField3.Value + ", " + HiddenField4.Value + ", " + HiddenField5.Value + ", " +
+                                   HiddenField6.Value + ")";
+                String inquiry3 = "INSERT INTO Flats VALUES(" + HiddenField7.Value + ", " + HiddenField8.Value + ", " +
+                                   HiddenField9.Value + ", " + HiddenField10.Value + ", " + HiddenField11.Value + ", " +
+                                   HiddenField12.Value + ")";
+
+                SqlCommand myCommand = new SqlCommand(inquiry1, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry2, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry3, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            else if (count == 5)
+            {
+                String inquiry1 = "INSERT INTO Flats VALUES(" + HiddenField25.Value + ", " + HiddenField26.Value + ", " +
+                                   HiddenField27.Value + ", " + HiddenField28.Value + ", " + HiddenField29.Value + ", " +
+                                   HiddenField30.Value + ")";
+                String inquiry2 = "INSERT INTO Flats VALUES(" + HiddenField1.Value + ", " + HiddenField2.Value + ", " +
+                                   HiddenField3.Value + ", " + HiddenField4.Value + ", " + HiddenField5.Value + ", " +
+                                   HiddenField6.Value + ")";
+                String inquiry3 = "INSERT INTO Flats VALUES(" + HiddenField7.Value + ", " + HiddenField8.Value + ", " +
+                                   HiddenField9.Value + ", " + HiddenField10.Value + ", " + HiddenField11.Value + ", " +
+                                   HiddenField12.Value + ")";
+                String inquiry4 = "INSERT INTO Flats VALUES(" + HiddenField13.Value + ", " + HiddenField14.Value + ", " +
+                                  HiddenField15.Value + ", " + HiddenField16.Value + ", " + HiddenField17.Value + ", " +
+                                  HiddenField18.Value + ")";
+
+                SqlCommand myCommand = new SqlCommand(inquiry1, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry2, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry3, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry4, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            else if (count == 6)
+            {
+                String inquiry1 = "INSERT INTO Flats VALUES(" + HiddenField25.Value + ", " + HiddenField26.Value + ", " +
+                                  HiddenField27.Value + ", " + HiddenField28.Value + ", " + HiddenField29.Value + ", " +
+                                  HiddenField30.Value + ")";
+                String inquiry2 = "INSERT INTO Flats VALUES(" + HiddenField1.Value + ", " + HiddenField2.Value + ", " +
+                                   HiddenField3.Value + ", " + HiddenField4.Value + ", " + HiddenField5.Value + ", " +
+                                   HiddenField6.Value + ")";
+                String inquiry3 = "INSERT INTO Flats VALUES(" + HiddenField7.Value + ", " + HiddenField8.Value + ", " +
+                                   HiddenField9.Value + ", " + HiddenField10.Value + ", " + HiddenField11.Value + ", " +
+                                   HiddenField12.Value + ")";
+                String inquiry4 = "INSERT INTO Flats VALUES(" + HiddenField13.Value + ", " + HiddenField14.Value + ", " +
+                                  HiddenField15.Value + ", " + HiddenField16.Value + ", " + HiddenField17.Value + ", " +
+                                  HiddenField18.Value + ")";
+                String inquiry5 = "INSERT INTO Flats VALUES(" + HiddenField19.Value + ", " + HiddenField20.Value + ", " +
+                                  HiddenField21.Value + ", " + HiddenField22.Value + ", " + HiddenField23.Value + ", " +
+                                  HiddenField24.Value + ")";
+
+                SqlCommand myCommand = new SqlCommand(inquiry1, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry2, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry3, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry4, myConnection);
+                myCommand.ExecuteNonQuery();
+                myCommand = new SqlCommand(inquiry5, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
             myConnection.Close();
 
             Response.Write("<script type='text/javascript'>");
             Response.Write("alert('Connected!!');");
             Response.Write("</script>");
-
-            return "I ate all of my lunch.";
         }
     }
 }
